fbd26f371518b38053cfe5866be0272e0516b969
Vincent Robert <vincent.robert@nanego.com>
2018-11-07 13:37:28 +0100

    Fix bug when there is less than 2 tabs in project settings

--- lib/redmine_admin_activity/projects_helper_patch.rb
+++ lib/redmine_admin_activity/projects_helper_patch.rb
@@ -7,7 +7,11 @@ module RedmineAdminActivity
     def project_settings_tabs
       tabs = super
       unregistered_watchers_tab = {name: 'admin_activity', action: :admin_activity, partial: 'projects/admin_activity', label: :project_module_admin_activity}
-      tabs.insert(2, unregistered_watchers_tab)
+      if tabs.size > 1
+        tabs.insert(2, unregistered_watchers_tab)
+      else
+        tabs << unregistered_watchers_tab
+      end
       tabs
     end
 
