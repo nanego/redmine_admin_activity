749de4be175ab648c13d4d1e93a0f4df08e4b67a
Nicolas Brousse <n.brousse@pantographe.studio>
2020-02-25 12:12:00 +0100

    track IssueCategory upgrades

--- lib/redmine_admin_activity/issue_categories_controller_patch.rb
+++ lib/redmine_admin_activity/issue_categories_controller_patch.rb
@@ -1,29 +1,48 @@
 require_dependency 'issue_categories_controller'
 
 class IssueCategoriesController
+  before_action :init_journal, :only => [:create, :update, :destroy]
   after_action :journalized_issue_categories_creation, :only => [:create]
+  after_action :journalized_issue_categories_upgrade, :only => [:update]
   after_action :journalized_issue_categories_deletion, :only => [:destroy]
+
+  private
+
+  def init_journal
+    @project.init_journal(User.current)
+  end
 
   def journalized_issue_categories_creation
     return unless @category.persisted?
 
-    @project.init_journal(User.current)
-    @project.current_journal.details << JournalDetail.new(
-        :property  => 'issue_category',
-        :prop_key  => 'issue_category',
+    add_journal_entry JournalDetail.new(
+        :property  => :issue_category,
+        :prop_key  => :issue_category,
         :value => @category.name
     )
-    @project.current_journal.save
+  end
+
+  def journalized_issue_categories_upgrade
+    return unless @category.name_previously_changed?
+
+    add_journal_entry JournalDetail.new(
+      :property  => :issue_category,
+      :prop_key  => :issue_category,
+      :value => @category.name,
+      :old_value => @category.name_previous_change[0]
+    )
   end
 
   def journalized_issue_categories_deletion
-    @project.init_journal(User.current)
-
-    @project.current_journal.details << JournalDetail.new(
-      :property  => 'issue_category',
-      :prop_key  => 'issue_category',
+    add_journal_entry JournalDetail.new(
+      :property  => :issue_category,
+      :prop_key  => :issue_category,
       :old_value => @category.name
     )
+  end
+
+  def add_journal_entry(journal_detail)
+    @project.current_journal.details << journal_detail
     @project.current_journal.save
   end
 end
