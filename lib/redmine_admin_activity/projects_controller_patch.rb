43288fb44023e50a58c5b26d049185efdbc6ac6e
Nicolas Brousse <n.brousse@pantographe.studio>
2020-03-03 17:21:03 +0100

    Track CustomFields changes

--- lib/redmine_admin_activity/projects_controller_patch.rb
+++ lib/redmine_admin_activity/projects_controller_patch.rb
@@ -1,26 +1,36 @@
 require_dependency 'projects_controller'
-require_dependency 'project'
 
 class ProjectsController
-
   before_action :init_journal, :only => [:update]
   after_action :update_journal, :only => [:update]
 
   def init_journal
     @project.init_journal(User.current)
     @previous_enabled_module_names = @project.enabled_module_names
+    @previous_enabled_issue_custom_field_ids = @project.issue_custom_field_ids
   end
 
   def update_journal
+    if @previous_enabled_issue_custom_field_ids != @project.issue_custom_field_ids
+      previous_custom_field_names = Tracker.where(:id => @previous_enabled_issue_custom_field_ids.map(&:to_i)).sorted.pluck(:name)
+
+      @project.current_journal.details << JournalDetail.new(
+        :property  => 'custom_fields',
+        :prop_key  => 'custom_fields',
+        :value => @project.issue_custom_fields.map(&:name).join(','),
+        :old_value => previous_custom_field_names.join(',')
+      )
+    end
+
     if @previous_enabled_module_names != @project.enabled_module_names
       @project.current_journal.details << JournalDetail.new(
-          :property  => 'modules',
-          :prop_key  => 'modules',
-          :old_value => @previous_enabled_module_names.join(','),
-          :value => @project.enabled_module_names.join(',')
+        :property  => 'modules',
+        :prop_key  => 'modules',
+        :value => @project.enabled_module_names.join(','),
+        :old_value => @previous_enabled_module_names.join(',')
       )
-      @project.current_journal.save
     end
-  end
 
+    @project.current_journal.save
+  end
 end
