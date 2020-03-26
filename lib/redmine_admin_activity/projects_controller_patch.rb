014eb2740512e716aeb0c7945b51142aaed98101
Vincent Robert <vincent.robert@nanego.com>
2020-03-26 16:52:28 +0100

    Merge Projects update_journal callbacks

--- lib/redmine_admin_activity/projects_controller_patch.rb
+++ lib/redmine_admin_activity/projects_controller_patch.rb
@@ -8,7 +8,6 @@ class ProjectsController
     @project.init_journal(User.current)
     @previous_enabled_module_names = @project.enabled_module_names
     @previous_enabled_tracker_ids = @project.tracker_ids
-    @previous_enabled_trackers = @project.trackers
     @previous_enabled_issue_custom_field_ids = @project.issue_custom_field_ids
   end
 
@@ -17,18 +16,19 @@ class ProjectsController
       previous_tracker_names = Tracker.where(:id => @previous_enabled_tracker_ids.map(&:to_i)).sorted.pluck(:name)
 
       @project.current_journal.details << JournalDetail.new(
-        :property  => 'trackers',
-        :prop_key  => 'trackers',
+        :property => 'trackers',
+        :prop_key => 'trackers',
         :value => @project.trackers.map(&:name).join(','),
         :old_value => previous_tracker_names.join(',')
       )
+    end
 
     if @previous_enabled_issue_custom_field_ids != @project.issue_custom_field_ids
       previous_custom_field_names = Tracker.where(:id => @previous_enabled_issue_custom_field_ids.map(&:to_i)).sorted.pluck(:name)
 
       @project.current_journal.details << JournalDetail.new(
-        :property  => 'custom_fields',
-        :prop_key  => 'custom_fields',
+        :property => 'custom_fields',
+        :prop_key => 'custom_fields',
         :value => @project.issue_custom_fields.map(&:name).join(','),
         :old_value => previous_custom_field_names.join(',')
       )
@@ -36,13 +36,13 @@ class ProjectsController
 
     if @previous_enabled_module_names != @project.enabled_module_names
       @project.current_journal.details << JournalDetail.new(
-        :property  => 'modules',
-        :prop_key  => 'modules',
+        :property => 'modules',
+        :prop_key => 'modules',
         :value => @project.enabled_module_names.join(','),
         :old_value => @previous_enabled_module_names.join(',')
       )
     end
 
-    @project.current_journal.save
+    @project.current_journal.save if @project.current_journal.details.any?
   end
 end
