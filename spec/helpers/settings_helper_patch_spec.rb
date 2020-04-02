75865fafadf39a7f0a6e5eb10128ace314defd2f
Baptiste Rasschaert <b.rasschaert@pantographe.studio>
2020-04-02 19:11:08 +0200

    Add test for settings_helper_patch (#88219)

---
+++ spec/helpers/settings_helper_patch_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe SettingsHelper, :type => :controller do
+
+  render_views
+
+  before do
+    @controller = SettingsController.new
+    @request    = ActionDispatch::TestRequest.create
+    @response   = ActionDispatch::TestResponse.new
+    User.current = nil
+    @request.session[:user_id] = 1
+  end
+
+  it "should display administration_settings_tabs_with_admin_activity" do
+    get :index
+
+    assert_response :success
+    assert_select "a[href='/settings?tab=admin_activity']"
+  end
+
+  it "should not display administration_settings_tabs_with_admin_activity" do
+    @request.session[:user_id] = 2
+
+    get :index
+
+    assert_response 403
+  end
+end
