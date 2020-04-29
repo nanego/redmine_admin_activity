b626d2607dcca33b24a042827a38c05a3f14eb21
Nicolas Brousse <n.brousse@pantographe.studio>
2020-04-29 16:38:57 +0200

    test: add test for member functions tracking

---
+++ spec/controllers/principal_memberships_controller_patch_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe PrincipalMembershipsController, type: :controller do
+  render_views
+
+  fixtures :projects, :users, :roles, :members, :member_roles, :issues, :issue_statuses, :versions,
+           :trackers, :projects_trackers, :issue_categories, :enabled_modules, :enumerations, :attachments,
+           :workflows, :custom_fields, :custom_values, :custom_fields_projects, :custom_fields_trackers,
+           :time_entries, :journals, :journal_details, :queries, :repositories, :changesets
+
+  if Redmine::Plugin.installed?(:redmine_limited_visibility)
+    fixtures :functions
+  end
+
+  include Redmine::I18n
+
+  before do
+    @controller = described_class.new
+    @request = ActionDispatch::TestRequest.create
+    @response = ActionDispatch::TestResponse.new
+    User.current = nil
+    @request.session[:user_id] = 1 #permissions are hard
+  end
+
+  let(:project1) { projects(:projects_001) }
+  let(:project2) { projects(:projects_002) }
+  let(:admin)    { users(:users_001) }
+  let(:user)     { users(:users_009) } # User Misc
+  let(:member)   { members(:members_001) } # User 2 (John Smith) member of Project 1 with 'Manager' role
+  let(:role)     { roles(:roles_002) } # Developer
+  let(:function) { functions(:functions_001) }
+
+  describe "POST /" do
+    it "adds a member to projects" do
+      post :create, params: { user_id: user.id, membership: { project_ids: [project1.id, project2.id], role_ids: [role.id] } }
+
+      expect(response).to redirect_to('http://test.host/users/9/edit?tab=memberships')
+      expect(project1.journals).to_not be_nil
+      expect(project2.journals).to_not be_nil
+
+      if Redmine::Plugin.installed?(:redmine_limited_visibility)
+        expect(project1.journals.last.details.last).to have_attributes(:value => "{\"name\":\"User Misc\",\"roles\":[\"Developer\"],\"functions\":[]}", :old_value => nil)
+        expect(project2.journals.last.details.last).to have_attributes(:value => "{\"name\":\"User Misc\",\"roles\":[\"Developer\"],\"functions\":[]}", :old_value => nil)
+      else
+        expect(project1.journals.last.details.last).to have_attributes(:value => "{\"name\":\"User Misc\",\"roles\":[\"Developer\"]}", :old_value => nil)
+        expect(project2.journals.last.details.last).to have_attributes(:value => "{\"name\":\"User Misc\",\"roles\":[\"Developer\"]}", :old_value => nil)
+      end
+    end
+  end
+
+  #   it "adds a role to a member" do
+  #     patch :update, params: { id: member.id, membership: { role_ids: member.roles.map(&:id) + [role.id] } }
+  #     expect(response).to redirect_to('/projects/ecookbook/settings/members')
+  #     expect(project.journals).to_not be_nil
+
+  #     if Redmine::Plugin.installed?(:redmine_limited_visibility)
+  #       expect(project.journals.last.details.last).to have_attributes(:value => nil, :old_value => "{\"name\":\"John Smith\",\"roles\":[\"Manager\"],\"functions\":[]}")
+  #     else
+  #       expect(project.journals.last.details.last).to have_attributes(:value => "{\"name\":\"John Smith\",\"roles\":[\"Manager\",\"Developer\"]}", :old_value => "{\"name\":\"John Smith\",\"roles\":[\"Manager\"]}")
+  #     end
+  #   end
+  # end
+
+  describe "DELETE /:id" do
+    it "removes a member" do
+      project1.update(functions: [function])
+      member = Member.new(:project => project1, :user => user)
+      member.set_editable_role_ids([roles(:roles_001).id], admin)
+      member.set_functional_roles([function.id]) if Redmine::Plugin.installed?(:redmine_limited_visibility)
+      member.save!
+
+      delete :destroy, params: { user_id: member.principal.id, id: member.id }
+      expect(response).to redirect_to('http://test.host/users/9/edit?tab=memberships')
+      expect(project1.journals).to_not be_nil
+
+      if Redmine::Plugin.installed?(:redmine_limited_visibility)
+        expect(project1.journals.last.details.last).to have_attributes(:value => nil, :old_value => "{\"name\":\"User Misc\",\"roles\":[\"Manager\"],\"functions\":[\"function1\"]}")
+      else
+        expect(project1.journals.last.details.last).to have_attributes(:value => nil, :old_value => "{\"name\":\"User Misc\",\"roles\":[\"Manager\"]}")
+      end
+    end
+  end
+end
--- spec/helpers/issues_helper_patch_spec.rb
+++ spec/helpers/issues_helper_patch_spec.rb
@@ -64,7 +64,6 @@ describe "IssuesHelperPatch" do
   end
 
   describe "members property" do
-
     describe "members property with legacy Array format" do
       it "should IssuesHelper#show_detail with no_html should show a changing enabled members" do
         detail = JournalDetail.new(:property => 'members', :old_value => ['user1'], :value => ['user1', 'user2'], :prop_key => 'members')
@@ -145,6 +144,45 @@ describe "IssuesHelperPatch" do
       end
     end
 
+    describe "members property with role and functions in JSON format" do
+      it "should IssuesHelper#show_detail with no_html should show a changing enabled members" do
+        detail = JournalDetail.new(:property => 'members', :old_value => '{"name":"user1","roles":["Developer"],"functions":[]}', :value => '{"name":"user1","roles":["Developer", "Manager"],"functions":["function1"]}', :prop_key => 'member_roles_and_functions')
+        expect(show_detail(detail, true)).to eq "Member user1 has been changed with roles from [Developer] to [Developer, Manager] and functions from [] to [function1]"
+      end
+
+      it "should IssuesHelper#show_detail with no_html should show a deleted members" do
+        detail = JournalDetail.new(:property => 'members', :old_value => '{"name":"user1","roles":["Developer"],"functions":["function1"]}', :value => nil, :prop_key => 'member_roles_and_functions')
+        expect(show_detail(detail, true)).to eq "Member user1, with roles [Developer] and functions [function1], has been removed"
+      end
+
+      it "should IssuesHelper#show_detail with html should show a new member with HTML highlights" do
+        detail = JournalDetail.new(:property => 'members', :old_value => nil, :value => '{"name":"user1","roles":["Developer"],"functions":["function2"]}', :prop_key => 'member_roles_and_functions')
+        detail.id = 1
+        result = show_detail(detail, false)
+        expect(result).to eq("Member user1 has been added with roles [Developer] and functions [function2]")
+      end
+
+      it "should IssuesHelper#show_detail with html should show a deleted members with HTML highlights" do
+        detail = JournalDetail.new(:property => 'members', :old_value => '{"name":"user1","roles":["Developer"],"functions":["function2"]}', :value => nil, :prop_key => 'member_roles_and_functions')
+        html = show_detail(detail, false)
+        expect(html).to eq("Member user1, with roles [Developer] and functions [function2], has been removed")
+      end
+
+      it "should IssuesHelper#show_detail with html should show all new members with HTML highlights" do
+        detail = JournalDetail.new(:property => 'members', :old_value => nil, :value =>  '{"name":"user1","roles":["Developer", "Manager"],"functions":["function3"]}', :prop_key => 'member_roles_and_functions')
+        detail.id = 1
+        result = show_detail(detail, false)
+        html = "Member user1 has been added with roles [Developer, Manager] and functions [function3]"
+        expect(result).to eq(html)
+      end
+
+      it "should IssuesHelper#show_detail with html should show all deleted members with HTML highlights" do
+        detail = JournalDetail.new(:property => 'members', :old_value =>  '{"name":"user1","roles":["Developer", "Manager"],"functions":["function1","function2"]}', :value => nil, :prop_key => 'member_roles_and_functions')
+        result = show_detail(detail, false)
+        html = "Member user1, with roles [Developer, Manager] and functions [function1, function2], has been removed"
+        expect(result).to eq(html)
+      end
+    end
   end
 
   describe "copy project" do
