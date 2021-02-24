2301ddf8a01b041891c8491be3e7ec5b0e05e96a
mathieu-mbru <C8JHwEYjT7magkecu2z7>
2021-02-24 10:23:20 +0100

    correction suite au squash

--- spec/helpers/journal_settings_helper_spec.rb
+++ spec/helpers/journal_settings_helper_spec.rb
@@ -30,23 +30,14 @@ describe "JournalSettingsHelper" do
   end
 
   describe "Project creation" do
-    it "should generate the right translated sentence for a project creation (project still exists)" do
-      project = projects(:projects_001)
-      journal = JournalSetting.new(:user_id => User.current.id,
-                                   :value_changes => project.previous_changes,
-                                   :journalized => project,
-                                   :journalized_entry_type => "create")
-      expect(project_update_text(journal)).to eq "Project <i><a href=\"/projects/ecookbook\">eCookbook</a></i> has been created."
-    end
-
-    it "should generate the right translated sentence for a project creation (project doesn't exist anymore)" do
+    it "should generate the right translated sentence for a project creation" do
       project = Project.new("name" => "Test create", "identifier" => "test-create")
+      project.save
       journal = JournalSetting.new(:user_id => User.current.id,
                                    :value_changes => { "name" => [nil, "eCookbook"] },
                                    :journalized => project,
-                                   :journalized_entry_type => "create")
-
-      expect(project_update_text(journal)).to eq "Project <i>eCookbook</i> has been created."
+                                   :journalized_entry_type => "create")      
+      expect(project_update_text(journal)).to eq "Project <i><a href=\"/projects/test-create\">Test create</a></i> has been created."
     end
   end
 
@@ -63,29 +54,19 @@ describe "JournalSettingsHelper" do
   end
 
   describe "Project duplication" do
-    it "should generate the right translated sentence for a project duplication (projects still exist)" do
-      project = projects(:projects_001)
-      source_project = projects(:projects_002)
-      journal = JournalSetting.new(:user_id => User.current.id,
-                                   :value_changes => { "source_project" => source_project.id,
-                                                       "source_project_name" => source_project.name },
-                                   :journalized => project,
-                                   :journalized_entry_type => "copy")
-
-      expect(project_update_text(journal)).to eq "Projet <i><a href=\"/projects/ecookbook\">eCookbook</a></i> has been copied from <i><a href=\"/projects/onlinestore\">OnlineStore</a></i>."
-    end
-
-    it "should generate the right translated sentence for a project duplication (projects don't exist anymore)" do
+    it "should generate the right translated sentence for a project duplication" do
       project = Project.new("name" => "Test create", "identifier" => "test-create")
+      project.save
       source_project = Project.new("name" => "Source project", "identifier" => "source-project")
       journal = JournalSetting.new(:user_id => User.current.id,
                                    :value_changes => { "name" => [nil, "eCookbook"],
                                                        "source_project" => 1000,
                                                        "source_project_name" => source_project.name },
                                    :journalized => project,
-                                   :journalized_entry_type => "copy")
-
-      expect(project_update_text(journal)).to eq "Projet <i>eCookbook</i> has been copied from <i>Source project</i>."
+                                   :journalized_entry_type => "copy")      
+      
+      source_project.save
+      expect(project_update_text(journal)).to eq "Projet <i><a href=\"/projects/test-create\">Test create</a></i> has been copied from <i>Source project</i>."
     end
   end
 
