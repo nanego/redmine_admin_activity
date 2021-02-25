ab94dc71ac1074180ddb884741580b1155bf92f2
mathieu-mbru <C8JHwEYjT7magkecu2z7>
2021-02-25 10:47:01 +0100

    Ajouter des dépendances users_controller

--- init.rb
+++ init.rb
@@ -9,7 +9,8 @@ ActiveSupport::Reloader.to_prepare do
   require_dependency 'redmine_admin_activity/controllers/custom_fields_controller'
   require_dependency 'redmine_admin_activity/controllers/settings_controller'
   require_dependency 'redmine_admin_activity/controllers/organizations/memberships_controller' if Redmine::Plugin.installed?(:redmine_organizations)
-
+  require_dependency 'redmine_admin_activity/controllers/users_controller'
+  
   require_dependency 'redmine_admin_activity/models/project'
   require_dependency 'redmine_admin_activity/models/member'
   require_dependency 'redmine_admin_activity/models/journal'
