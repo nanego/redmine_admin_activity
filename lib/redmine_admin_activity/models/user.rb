807538d9d5da46a0ea0a4a85fd8a44d8aa07d892
mathieu-mbru <m.brulin@adacis.net>
2022-02-24 08:48:23 +0100

    tracer les modification de user

--- lib/redmine_admin_activity/models/user.rb
+++ lib/redmine_admin_activity/models/user.rb
@@ -4,4 +4,30 @@ class User < Principal
 
 	has_many :JournalSetting, :dependent => :nullify
 
+	has_many :journals, :as => :journalized, :dependent => :destroy, :inverse_of => :journalized
+
+	attr_reader :current_journal
+
+	after_save :create_journal
+
+	def init_journal(user)
+		@current_journal ||= Journal.new(:journalized => self, :user => user)
+	end
+
+	# Returns the current journal or nil if it's not initialized
+  def current_journal
+    @current_journal
+  end
+
+  # Returns the names of attributes that are journalized when updating the user
+  def journalized_attribute_names
+    names = User.column_names - %w(id hashed_password language salt staff beta_tester trusted_api_user created_on updated_on) + ["mails"]
+  end
+
+	def create_journal
+		if current_journal
+			current_journal.save
+		end
+	end
+
 end
