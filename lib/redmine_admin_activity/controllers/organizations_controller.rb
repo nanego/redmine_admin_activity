00d97acef68f69dd771160f527e2efa54c26233b
mathieu-mbru <m.brulin@adacis.net>
2022-03-17 15:30:57 +0100

    tracer les modifications d'orgainsation

--- lib/redmine_admin_activity/controllers/organizations_controller.rb
+++ lib/redmine_admin_activity/controllers/organizations_controller.rb
@@ -5,6 +5,8 @@ class OrganizationsController < ApplicationController
   after_action :journalized_organizations_creation, :only => [:create]
   after_action :journalized_organizations_deletion, :only => [:destroy]
   before_action :memorize_deleted_organizations, :only => [:destroy]
+  before_action :memorize_attributes_organizations, :only => [:update]
+  after_action :journalized_organizations_update, :only => [:update]
 
   private
 
@@ -41,5 +43,26 @@ class OrganizationsController < ApplicationController
 
   def memorize_deleted_organizations
     @deleted_organizations = @organization.self_and_descendants.to_a
+  end
+
+  def memorize_attributes_organizations
+    @old_attributes = @organization.attributes
+  end
+
+  def journalized_organizations_update
+    # Build hash of previous_changes manually, After a long search, I couldn't know why the previous changes are empty when changing the parent.
+    @previous_changes = {}
+    @new_attributes = @organization.attributes
+    @new_attributes.each do |key, val|
+      @previous_changes[key] =
+          [@old_attributes[key], val] if @organization.journalized_attribute_names.include?(key) && @old_attributes[key] != val
+    end
+
+    JournalSetting.create(
+      :user_id => User.current.id,
+      :value_changes => @previous_changes,
+      :journalized => @organization,
+      :journalized_entry_type => "update",
+    )
   end
 end
