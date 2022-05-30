b9270f07aa0081b7a57e6e6f09ee8c91ce0ff86e
Vincent Robert <vincent.robert@nanego.com>
2022-05-30 08:55:48 +0200

    Specify foreign_key type in db migration

--- db/migrate/20200331162520_create_journal_settings.rb
+++ db/migrate/20200331162520_create_journal_settings.rb
@@ -2,7 +2,7 @@ class CreateJournalSettings < ActiveRecord::Migration[5.2]
   def change
     create_table :journal_settings do |t|
       t.json :value_changes, :null => false
-      t.references :user, :foreign_key => true, :null => false
+      t.references :user, :foreign_key => true, :null => false, type: :integer
       t.datetime :created_on, :null => false
     end unless ActiveRecord::Base.connection.table_exists? 'journal_settings'
   end
